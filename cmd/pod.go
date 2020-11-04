d6b9f41f7ef49c704178f319998814e46a31dbbe
disaster37 <linuxworkgroup@hotmail.com>
2020-11-04 15:44:38 +0100

    Add command to clean evicted pods (#12)
    * Add log output when run job. Add ability to run pre/post job on given namespace
    
    * Add command to clean evicted pods
    
    * Add new command on readme
    
    Co-authored-by: LANGOUREAUX Sebastien <Sebastien.LANGOUREAUX@harmonie-mutuelle.fr>

---
+++ cmd/pod.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"context"
+	"os"
+
+	"github.com/disaster37/kubetool/v1.18/kubetool"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+)
+
+// CleanEvictedPods permit to remove failed pods because of evicted
+func CleanEvictedPods(c *cli.Context) error {
+	cmd, err := newCmd(c)
+	if err != nil {
+		log.Errorf("Can't connect on kubernetes: %s", err.Error())
+		os.Exit(1)
+	}
+
+	ctx, cancelFunc := getContext(c)
+	if cancelFunc != nil {
+		defer cancelFunc()
+	}
+
+	err = cleanEvictedPods(ctx, cmd)
+	if err != nil {
+		return err
+	}
+
+	log.Infof("Clean evicted pods finished successfully")
+	return nil
+}
+
+func cleanEvictedPods(ctx context.Context, cmd *kubetool.Kubetool) (err error) {
+
+	// Get post job
+	err = cmd.CleanEvictedPods(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ cmd/pod_test.go
@@ -0,0 +1,59 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/disaster37/kubetool/v1.18/kubetool"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/version"
+	discoveryfake "k8s.io/client-go/discovery/fake"
+	"k8s.io/client-go/kubernetes/fake"
+	k8stesting "k8s.io/client-go/testing"
+)
+
+func (s *TestSuite) TestCleanEvictedNodes() {
+
+	fakeClient := fake.NewSimpleClientset()
+	fakeClient.Fake = fake.Clientset{}.Fake
+	fakeDiscovery := fakeClient.Discovery().(*discoveryfake.FakeDiscovery)
+	fakeDiscovery.FakedServerVersion = &version.Info{
+		Major: "1",
+		Minor: "18",
+	}
+
+	// Mock list pod
+	fakeClient.Fake.AddReactor("list", "pods", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+		pods := &v1.PodList{
+			Items: []v1.Pod{
+				{
+					Spec: v1.PodSpec{
+						NodeName: "fake-node",
+					},
+					ObjectMeta: meta.ObjectMeta{
+						Name:      "fake-pod",
+						Namespace: "fake-namespace",
+					},
+				},
+			},
+		}
+		return true, pods, nil
+	})
+
+	// Mock delete pod
+	fakeClient.Fake.AddReactor("delete", "pods", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+		return true, nil, nil
+	})
+
+	// Trap all
+	fakeClient.Fake.AddReactor("*", "*", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+		return true, nil, fmt.Errorf("no reaction implemented for %s", action)
+	})
+	cmd := kubetool.NewConnexionFromClient(fakeClient)
+
+	err := cleanEvictedPods(context.Background(), cmd)
+	assert.NoError(s.T(), err)
+}
