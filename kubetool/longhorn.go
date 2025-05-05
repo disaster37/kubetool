4c7c58dfa521d0ccb19705efb054e299b0e9a3a6
disaster37 <linuxworkgroup@hotmail.com>
2025-05-05 09:49:36 +0000

    fix: fix lonhgorn

--- kubetool/longhorn.go
+++ kubetool/longhorn.go
@@ -5,6 +5,7 @@ import (
 
 	"emperror.dev/errors"
 	longhorn "github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta2"
+	"github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/discovery"
 	"k8s.io/kubectl/pkg/scheme"
@@ -27,10 +28,15 @@ func (k *Kubetool) CleanPendingBackup(ctx context.Context) (err error) {
 		return errors.Wrap(err, "Error when list Longhorn backups")
 	}
 
+	s := scheme.Scheme
+	if err := longhorn.AddToScheme(s); err != nil {
+		panic(err)
+	}
+
 	for _, backupUnstructuredObj := range list.Items {
 		// Need to convert to Backup type
 		backup := &longhorn.Backup{}
-		if err := scheme.Scheme.Convert(backupUnstructuredObj, backup, nil); err != nil {
+		if err := s.Convert(&backupUnstructuredObj, backup, nil); err != nil {
 			return errors.Wrap(err, "Error when convert to Longhorn Backup type")
 		}
 
@@ -39,6 +45,7 @@ func (k *Kubetool) CleanPendingBackup(ctx context.Context) (err error) {
 			if err != nil {
 				return errors.Wrap(err, "Error when delete Longhorn backup on pending state")
 			}
+			logrus.Infof("Longhorn backup %s deleted", backup.Name)
 		}
 	}
 
